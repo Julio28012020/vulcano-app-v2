6d939523d09b4f8fcd77651a15cc3a8db12e4239
Albany Luciani <lucianialbany@gmail.com>
2026-04-11 14:11:55 -0500

    Add Review page and integrate routing for reviews

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import VulcanHome from "../pages/VulcanHome"
 import VulcanoLogin from "../components/VulcanoLogin";
-import {Page404} from "../pages/Page404"
+import Review from "../pages/Review";
+import { Page404 } from "../pages/Page404"
 
 export const MyRoutes = () => (
     <BrowserRouter>
-    <Routes>
-        <Route path="/" element={<VulcanHome/>}/>
-        <Route path="/Login" element={<VulcanoLogin/>}/>
-        <Route path="/Review" />
-        <Route path="*" element={<Page404/>}/>
-    </Routes>
+        <Routes>
+            <Route path="/" element={<VulcanHome/>}/>
+            <Route path="/Login" element={<VulcanoLogin/>}/>
+            <Route path="/Review" element={<Review/>}/>
+            <Route path="*" element={<Page404/>}/>
+        </Routes>
     </BrowserRouter>
 )
